ebd70470a0e3c39436abbe5d7af53bb3b2238a6a
ljbennett62 <lbenn@us.ibm.com>
2022-01-10 13:15:23 -0800

    initial add

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# pact-online-course
+# Pact Online Course
+
+## Running the tests
+
+1. `npm install`
+2. `npm run pacts` - Run consumer tests
+
+## Publish Contracts
+
+1. `npm run publish`
---
+++ tests/consumer.pact.js
@@ -0,0 +1,59 @@
+const path = require("path")
+const chai = require("chai")
+const { Pact, Matchers } = require("@pact-foundation/pact")
+const chaiAsPromised = require("chai-as-promised")
+const expect = chai.expect
+chai.use(chaiAsPromised)
+const { string } = Matchers
+const get = require('../src/get')
+
+describe('Consumer Test', () => {
+
+    it('OK response', () => {
+        get()
+        .then((response) => {
+            //console.log(response)
+            expect(response.statusText).to.be.equal('OK ')
+        })
+    })
+
+    let provider = Pact;
+    before(() => {
+        provider = new Pact({
+            consumer: "React",
+            provider: "token",
+            port: 1235,
+            log: path.resolve(process.cwd(), 'logs', 'pact.log'),
+            dir: path.resolve(process.cwd(), 'pacts'),
+            logLevel: "INFO"
+         });
+
+         return provider.setup()
+
+    }, 30000);
+
+    after(() => provider.finalize());
+
+
+    beforeEach(() => {
+        return provider.addInteraction({
+            state: "user token",
+            uponReceiving: "GET user token",
+            withRequest: {
+                method: "GET",
+                path: "/token/1235",
+                headers: { Accept: "application/json, text/plain, */*" }
+            },
+            willRespondWith: {
+                headers: { "Content-Type": "application/json" },
+                status: 200,
+                body: { "token": string("bearer") }
+            }
+       })
+    })
+
+    afterEach(() => provider.verify());
+
+  
+
+})
